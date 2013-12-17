9cf7f4fe0d58c9ce6b686b2b5bfcecd6bf6474d5
Jeremy Olliver <jeremy.olliver@gmail.com>
2013-12-18 11:54:08 +1300

    Bump version to 0.2.3

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "jeremy.olliver@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures application_balancer"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.2"
+version          "0.2.3"
 
 depends "haproxy2"
 recommends "rsyslog"
